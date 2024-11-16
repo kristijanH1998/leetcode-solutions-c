67916168c6d56d3a9f2011e1456f785bc4ff51e4
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-11-15 19:41:43 -0800

    algorithm performs correctly, prints output to console, but output should be stored in returned array

--- letter_combs_of_phone_num/letter_combs_of_phone_num.c
+++ letter_combs_of_phone_num/letter_combs_of_phone_num.c
@@ -4,53 +4,68 @@
 
 char* createCombination(int digits, int mappingIndex, char* combination, char** mappings) {
     
-//digits starts at number of digits in input
-//mappingIndex starts at 0  
-
+    //digits starts at number of digits in input
+    //mappingIndex starts at 0  
+    // char *current_combination = (char*) malloc (((sizeof(combination) / sizeof(char)) + 1) * sizeof(char));
+    // current_combination[]
+    
     if(digits > 0) {
-        if(digits == (sizeof(mappings) / sizeof(char*))) {
-            char *combination = (char*) malloc (digits * sizeof(char));
-        }
+        // if(digits == (sizeof(mappings) / sizeof(char*))) {
+        //     char *combination = (char*) malloc (digits * sizeof(char));
+        // }
 
         for(int i = 0; i < strlen(mappings[mappingIndex]); i++){
             // printf("%c", mappings[mappingIndex][i]);
             combination[strlen(combination)] = mappings[mappingIndex][i];
 
-            // char *combination_new = (char*) malloc (1 * sizeof(char));
+            // combination = (char*) realloc(combination, (i + 2) * sizeof(char));
+
+            // char *combination_new = (char*) malloc ((strlen(combination) / sizeof(char)) * sizeof(char));
+            // strcpy(combination_new, combination);
+
             createCombination(digits - 1, mappingIndex + 1, combination, mappings);
-            printf("\n");
+            combination[strlen(combination) - 1] = '\0';
         }
         
     } else {
         printf("%s\n", combination);
+        // combination[strlen(combination) - 1] = '\0';
+
+        // if(digits ==)
+
+        // memset(combination, 0, (strlen(combination) / sizeof(char)));
+        // strcpy(combination, "");
+        // combination = (char*) realloc(combination, sizeof(char));
+        // *combination = '\0';
+        // free(combination);
+        // char *combination = (char*) malloc (sizeof(char));
         return combination;
     }
-    
-
     return "";
 }
 
 char** letterCombinations(char* digits, int* returnSize) {
-    char *mappings[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    // printf("%c\n", mapping[0][2]);
+    char *base_mappings[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    char **mappings = (char**) malloc (sizeof(char*));
 
     for(int i = 0; i < (*returnSize); i++) {
-        printf("%c\n", mappings[(*(digits + i) - '2')][0]);
+        *(mappings + i) = base_mappings[(*(digits + i) - '2')];
+        mappings = (char**) realloc (mappings, (i + 2) * sizeof(char*));
     }
-    printf("%d\n", strlen(mappings[0]));
+
+
+    char *combination = (char*) malloc (*returnSize * sizeof(char));
+    createCombination(*returnSize, 0, combination, mappings);
 
     return NULL;
 }
 
 int main() {
-    char *digits = "2342";
+    char *digits = "232";
     int digits_size = strlen(digits);
-    // letterCombinations(digits, &digits_size);
-
-
-    char *mappings[] = {"abc", "def"};
-    
-    createCombination(2, 0, NULL, mappings);
+    letterCombinations(digits, &digits_size);
+    // char *mappings[] = {"abc", "def"};
+    // createCombination(2, 0, NULL, mappings);
     // printf("%d\n", sizeof(mappings) / sizeof(char*));
     return 0;
  }
