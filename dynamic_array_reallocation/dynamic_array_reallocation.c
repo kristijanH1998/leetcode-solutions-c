f7c745dff608afdcfe394d35131e3909ddc1f643
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-11-20 02:53:28 -0800

    dynamic array edit

--- dynamic_array_reallocation/dynamic_array_reallocation.c
+++ dynamic_array_reallocation/dynamic_array_reallocation.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** func(int** arr) {
+//Code below doesn't work:
+// int** func(int** arr) {
+//     for(int i = 1; i <= 5; i++) {
+//         *arr = (int*) realloc (*arr, (i + 1) * sizeof(int));
+//         *(*(arr + (i - 1))) = i;
+//     }
+//     return arr;
+// }
+
+// int main() {
+//     int *arr = (int*) malloc (1 * sizeof(int));
+//     int **res = func(&arr);
+//     for(int i = 0; i < 5; i++) {
+//         printf("%d ", *(*(res + i)));
+//     }
+//     return 0;
+// }
+
+//This works:
+int* func(int** arr) {
+    int *result = *arr;
+
     for(int i = 1; i <= 5; i++) {
-        *arr = (int*) realloc (*arr, (i + 1) * sizeof(int));
-        *(*(arr + (i - 1))) = i;
+        result = (int*) realloc (result, (i + 1) * sizeof(int));
+        *(result + (i - 1)) = i;
     }
-    return arr;
+   
+    return result;
 }
+
 
 int main() {
     int *arr = (int*) malloc (1 * sizeof(int));
-    int **res = func(&arr);
+    int *res = func(&arr);
     for(int i = 0; i < 5; i++) {
-        printf("%d ", *(*(res + i)));
+        printf("%d ", *(res + i));
     }
+
     return 0;
 }
