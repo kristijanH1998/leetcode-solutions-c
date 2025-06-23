80690689c2db1d3a036803557d52b072eb6ab913
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-06-23 06:38:42 -0700

    4sum

--- 4sum/solution.c
+++ 4sum/solution.c
@@ -8,26 +8,52 @@
  */
 int** fourSum(int* nums, int numsSize, int target, int* returnSize, int** returnColumnSizes) {
     int i, j, k, l;
+    if(nums == NULL || numsSize < 1) {
+        return NULL;
+    }
+    int retSz = *returnSize;
     for(i = 0; i <= (numsSize - 4); i++) {
         for(j = i + 1; j <= (numsSize - 3); j++) {
             for(k = j + 1; k <= (numsSize - 2); k++) {
                 for(l = k + 1; l <= (numsSize - 1); l++) {
-                    // printf("%d %d %d %d\n", nums[i], nums[j], nums[k], nums[l]);
                     if((nums[i] + nums[j] + nums[k] + nums[l]) == target) {
-                        printf("%d %d %d %d\n", nums[i], nums[j], nums[k], nums[l]);
+                        *(*(returnColumnSizes + retSz) + 0) = nums[i];
+                        *(*(returnColumnSizes + retSz) + 1) = nums[j];
+                        *(*(returnColumnSizes + retSz) + 2) = nums[k];
+                        *(*(returnColumnSizes + retSz) + 3) = nums[l];
+                        // returnColumnSizes[retSz][0] = nums[i];
+                        // returnColumnSizes[retSz][1] = nums[j];
+                        // returnColumnSizes[retSz][2] = nums[k];
+                        // returnColumnSizes[retSz][3] = nums[l];
+                        retSz++;
+                        returnColumnSizes = (int**)realloc(returnColumnSizes, (retSz + 1) * sizeof(int *));
+                        *(returnColumnSizes + retSz) = (int *)malloc(4 * sizeof(int));
                     }
                 }
             }
         }
     }
-    return NULL;
+    *returnSize = retSz;
+    return returnColumnSizes;
 }
 
 int main(void) {
     int *nums = (int *)malloc(10 * sizeof(nums));
-    for(int i = 0; i < 10; i++) {
+    int i;
+    for(i = 0; i < 10; i++) {
         *(nums + i) = i + 1;
     }
-    fourSum(nums, 10, 20, NULL, NULL);
+    int **quadruplets = (int **)malloc(1 * sizeof(int *));
+    *quadruplets = (int *)malloc(4 * sizeof(int));
+    int returnSize = 0;
+    int *returnSizePtr = &returnSize;
+    quadruplets = fourSum(nums, 10, 30, returnSizePtr, quadruplets);
+    for(i = 0; i < returnSize; i++) {
+        printf("%d %d %d %d\n", quadruplets[i][0], quadruplets[i][1], quadruplets[i][2], quadruplets[i][3]);
+        free(*(quadruplets + i));
+        *(quadruplets + i) = NULL;
+    }
+    free(quadruplets);
+    quadruplets = NULL;
     return 0;
 }
