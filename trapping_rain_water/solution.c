bb23811c481235d48d0eadefa4fcc739effd1071
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-01 12:32:39 -0700

    trapping water

--- trapping_rain_water/solution.c
+++ trapping_rain_water/solution.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define MIN(a,b) (((a) <= (b)) ? (a) : (b))
 
+// sub-optimal solution: 
 int trap(int *height, int heightSize) {
     if((height == NULL) || (heightSize <= 0)) {
         return 0;
@@ -35,6 +37,57 @@ int trap(int *height, int heightSize) {
     }
     return water;
 }
+
+// second attempt:
+// int trap(int *height, int heightSize) {
+//     if((height == NULL) || (heightSize <= 0)) {
+//         return 0;
+//     }
+//     int i, j, maxLeftIndex, maxRightIndex;
+//     int highest, maxLeft, maxRight;
+//     highest = maxLeft = maxRight = INT_MIN;
+//     int highestIndex = 0;
+//     // finding the tallest index
+//     for(i = 0; i < heightSize; i++) {
+//         if(height[i] > highest) {
+//             highest = height[i];
+//             highestIndex = i;
+//         }
+//     }
+//     // finding next tallest indices to the left and right of the tallest index
+//     // continue to look for next tallest indices to the left and right, adding water according to the formula from sub-optimal solution
+//     int tempIndex = highestIndex - 1;
+//     int water = 0;
+//     while(tempIndex > 0) {
+//         for(i = tempIndex; i >= 0; i--) {
+//             if(height[i] > maxLeft) {
+//                 maxLeft = height[i];
+//                 maxLeftIndex = i;
+//             }
+//         }
+//         while(tempIndex > maxLeftIndex) {
+//             water = water + (maxLeft - height[tempIndex]);
+//             tempIndex--;
+//         }
+//         maxLeft = INT_MIN;
+//     }
+//     tempIndex = highestIndex + 1;
+//     while(tempIndex < (heightSize - 1)) {
+//         for(j = tempIndex; j <= (heightSize - 1); j++) {
+//             if(height[j] > maxRight) {
+//                 maxRight = height[j];
+//                 maxRightIndex = j;
+//             }
+//         }
+//         while(tempIndex < maxRightIndex) {
+//             water = water + (maxRight - height[tempIndex]);
+//             tempIndex++;
+//         }
+//         maxRight = INT_MIN;
+//     }
+//     printf("%d\n", water);
+//     return water;
+// }
 
 int main(void) {
     int heights[] = {0,1,0,2,1,0,1,3,2,1,2,1};
