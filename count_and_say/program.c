221ed0bf6823575080b69fcdc5bfc6cd86366b59
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-09 15:31:34 -0700

    first algorithm, doesn't free heap memory

---
+++ count_and_say/program.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+char *count_and_say(int n) {
+    if(n < 1 || n > 30) {
+        printf("Invalid input: %s", strerror(errno));
+        return "";
+    }
+    if(n == 1) {
+        return "1";
+    }
+    char *rleStr = count_and_say(n - 1);
+    size_t rleLen = strlen(rleStr);
+    char *rleRes = (char *)malloc(2 * rleLen);
+    int count = 1;
+    int i, j;
+    for(i = 0, j = 0; i < rleLen; i++) {
+        if(*(rleStr + i + 1) == *(rleStr + i)) {
+            count++;
+        } else {
+            *(rleRes + (j++)) = (count + '0');
+            *(rleRes + (j++)) = *(rleStr + i);
+            count = 1;
+        }
+    }
+    rleRes[j] = '\0';
+    return rleRes;
+}
+
+int main(void) {
+    char *res;
+    for(int i = 1; i <= 30; i++) {
+        res = count_and_say(i);
+        printf("%s\n\n", res);
+    }
+    free(res);
+    // printf("%s\n", count_and_say(-1));
+    return 0;
+}
