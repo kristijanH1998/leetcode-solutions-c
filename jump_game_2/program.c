0a7dde6df7f14b07433538c057d292790acee880
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-10 00:44:35 -0700

    first solution, not yet tested on LeetCode

---
+++ jump_game_2/program.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+// static int minJumps = INT_MAX;
+
+int find_jumps(int *nums, int numsSize, int jumps) {
+    if(nums == NULL) {
+        return -1;
+    }
+    if(numsSize == 1) {
+        return jumps;
+    }
+    int minJumps = INT_MAX;
+    int temp;
+    for(int i = 1; i <= nums[0]; i++) {
+        temp = find_jumps(nums + i, numsSize - i, jumps + 1);
+        if(temp < minJumps) {
+            minJumps = temp;
+        }
+    }
+    return minJumps;
+}
+
+int jump(int *nums, int numsSize) {
+    if(nums == NULL || numsSize < 1 || numsSize > pow(10, 4)) {
+        return -1;
+    }
+    if(numsSize == 1) {
+        return 0;
+    }
+    int jumps = 0;
+    int i;
+    int temp;
+    int minJumps = INT_MAX;
+    for(i = 1; i <= nums[0]; i++) {
+        temp = find_jumps(nums + i, numsSize - i, jumps + 1);
+        if(temp < minJumps) {
+            minJumps = temp;
+        }
+    }
+    return minJumps;
+}
+
+int main(void) {
+    int nums[5] = {1, 1, 2, 1, 3};
+    int numsSize = sizeof(nums) / sizeof(int);
+    printf("%d\n", jump(nums, numsSize));
+    return 0;
+}
