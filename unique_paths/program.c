701ed7a15df2954805d6ce313924e1680a18b355
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-11 21:17:56 -0700

    recursive algorithm, takes too long to compute

---
+++ unique_paths/program.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RIGHT "R"
+#define DOWN "D"
+
+void generate_paths(int m, int n, char *move, int *count) {
+    if(m == 0 && n == 0) {
+        (*count)++;
+        // printf("\n");
+        return;
+    }
+    // printf("%s ", move);
+    if(m > 0) {
+        generate_paths(m - 1, n, DOWN, count);
+    }
+    if(n > 0) {
+        generate_paths(m, n - 1, RIGHT, count);
+    }
+    return;
+} 
+
+// m - 1 downs and n - 1 rights
+int unique_paths(int m, int n) {
+    // printf("%d %d\n", m, n);
+    if(m == 1 || n == 1) {
+        return 1;
+    }
+    int count = 0;
+    generate_paths(m - 2, n - 1, DOWN, &count);
+    generate_paths(m - 1, n - 2, RIGHT, &count);
+    // printf("%d\n", count);
+    return count;
+}
+
+int main(void) {
+    // printf("%s\n", RIGHT);
+    // int (*fptr)(int, int);
+    // fptr = &uniquePaths;
+    // (*fptr)(3, 4);
+    printf("%d\n", unique_paths(51, 9));
+    return 0;
+}
