0baadc642501face3c476d720c0a2bc1d97febe1
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-13 18:37:46 -0700

    working solution, needs more testing and handling of cases when rows == 1 or cols == 1 (or 0 in either case)

---
+++ spiral_matrix/program.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct position {
+    int x;
+    int y;
+} pos = {0, 0};
+
+int *spiralOrder(int **matrix, int matrixSize, int *matrixColSize, int *returnSize) {
+    int rows = matrixSize;
+    int cols = *matrixColSize;
+    int matLen = rows * cols;
+    int *spiral = (int*)malloc((matLen + 1) * sizeof(int));
+    int limitRL = 0;
+    int limitUD = 0;
+    int copied = 0;
+    while(copied < matLen) {
+        while(pos.y < (cols - limitRL)) {
+            *(spiral + (copied++)) = *(*(matrix + pos.x) + pos.y);
+            (pos.y)++;
+        }
+        (pos.y)--;
+        (pos.x)++;
+        while(pos.x < (rows - limitUD)) {
+            *(spiral + (copied++)) = *(*(matrix + pos.x) + pos.y);
+            (pos.x)++;
+        }
+        (pos.x)--;
+        (pos.y)--;
+        limitUD++;
+        while(pos.y >= limitRL) {
+            *(spiral + (copied++)) = *(*(matrix + pos.x) + pos.y);
+            (pos.y)--;
+        }
+        (pos.y)++;
+        (pos.x)--;
+        limitRL++;
+        while(pos.x >= limitUD) {
+            *(spiral + (copied++)) = *(*(matrix + pos.x) + pos.y);
+            (pos.x)--;
+        }
+        (pos.x)++;
+        (pos.y)++;
+    }
+    *returnSize = (rows == cols) ? copied : (copied - 1);
+    return spiral;
+}
+
+int main(void) {
+    int rows = 2;
+    int cols = 2;
+    int **matrix = (int**)malloc(rows * sizeof(int *));
+    int i;
+    for(i = 0; i < rows; i++) {
+        *(matrix + i) = (int*)malloc(cols * sizeof(int));
+    }
+    int j;
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            *(*(matrix + i) + j) = i * cols + (j + 1);
+        }
+    }
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", *(*(matrix + i) + j));
+        }
+        printf("\n");
+    }
+    int returnSize = 0;
+    int *spiral = spiralOrder(matrix, rows, &cols, &returnSize);
+    printf("%d\n", returnSize);
+    for(i = 0; i < returnSize; i++) {
+        printf("%d ", *(spiral + i));
+    }
+    printf("\n");
+    return 0;
+}
