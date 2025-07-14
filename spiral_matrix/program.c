56a4f9612f398761fb370ff777d3f81d4bf10e3c
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-07-13 18:52:28 -0700

    handling of special dimensions of matrix

--- spiral_matrix/program.c
+++ spiral_matrix/program.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct position {
     int x;
@@ -7,10 +8,21 @@ struct position {
 } pos = {0, 0};
 
 int *spiralOrder(int **matrix, int matrixSize, int *matrixColSize, int *returnSize) {
+    if(matrixSize <= 0 || (*matrixColSize) <= 0) {
+        return NULL;
+    }
     int rows = matrixSize;
     int cols = *matrixColSize;
     int matLen = rows * cols;
     int *spiral = (int*)malloc((matLen + 1) * sizeof(int));
+    if(rows == 1) {
+        memcpy(spiral, matrix[0], matLen);
+    }
+    if(cols == 1) {
+        for(int i = 0; i < rows; i++) {
+            spiral[i] = matrix[i][0];
+        }
+    }
     int limitRL = 0;
     int limitUD = 0;
     int copied = 0;
