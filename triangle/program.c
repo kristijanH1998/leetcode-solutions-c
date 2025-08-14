2d85fcd1ad7c98413875f0ff80ed49231c7535b1
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-08-13 18:03:26 -0700

    recursive solution

---
+++ triangle/program.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void findSum(int **triangle, int triangleSize, int row, int numIndex, int pathSum, int *minSum) {
+    // printf("%d ", triangle[row][numIndex]);
+    pathSum += triangle[row][numIndex];
+    if(pathSum > *minSum) {
+        return;
+    } 
+    if((row == (triangleSize - 1))) {
+        if(pathSum < *minSum) {
+            *minSum = pathSum;
+        }
+        // printf("\n");
+        return;
+    }
+    // if() {
+    //     if(pathSum < *minSum) {       
+    //     }
+    //     return;
+    // }
+    findSum(triangle, triangleSize, row + 1, numIndex, pathSum, minSum);
+    findSum(triangle, triangleSize, row + 1, numIndex + 1, pathSum, minSum);
+    // return pathSum + ;
+}
+
+int minimumTotal(int **triangle, int triangleSize, int *triangleColSize) {
+    // int i;
+    // for(i = 0; i < triangleSize; i++) {
+    //     printf("%d ", triangleColSize[i]);
+    // }
+    // printf("\n");
+    // int j;
+    // for(i = 0; i < triangleSize; i++) {
+    //     for(j = 0; j < triangleColSize[i]; j++) {
+    //         printf("%d ", triangle[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    int minSum = INT_MAX;
+    findSum(triangle, triangleSize, 0, 0, 0, &minSum);
+    return minSum;
+}
+
+int main() {
+    int rows = 4;
+    int **triangle = (int**)malloc(rows * sizeof(int*));
+    int numbers = 1;
+    int triangleColSize[rows];
+    for(numbers = 1; numbers <= rows; numbers++) {
+        *(triangle + (numbers - 1)) = (int*)malloc(numbers * sizeof(int));
+        triangleColSize[numbers - 1] = numbers;
+    }
+    *(*(triangle + 0) + 0) = 2;
+    *(*(triangle + 1) + 0) = 3;
+    *(*(triangle + 1) + 1) = 4;
+    *(*(triangle + 2) + 0) = 6;
+    *(*(triangle + 2) + 1) = 5;
+    *(*(triangle + 2) + 2) = 7;
+    *(*(triangle + 3) + 0) = 4;
+    *(*(triangle + 3) + 1) = 1;
+    *(*(triangle + 3) + 2) = 8;
+    *(*(triangle + 3) + 3) = 3;
+    printf("%d\n", minimumTotal(triangle, rows, triangleColSize));
+    return 0;
+}
