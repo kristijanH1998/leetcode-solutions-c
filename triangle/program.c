17fdc81e5643d49392ad982e9437b45ecf7ce03d
kristijanH1998 <kristijan.hornung1@gmail.com>
2025-08-14 23:36:51 -0700

    DP solution

--- triangle/program.c
+++ triangle/program.c
@@ -2,51 +2,69 @@
 #include <stdlib.h>
 #include <limits.h>
 
-void findSum(int **triangle, int triangleSize, int row, int numIndex, int pathSum, int *minSum) {
-    // printf("%d ", triangle[row][numIndex]);
-    pathSum += triangle[row][numIndex];
-    // if(pathSum > *minSum) {
-    //     return;
-    // } 
-    if((row == (triangleSize - 1))) {
-        if(pathSum < *minSum) {
-            *minSum = pathSum;
-        }
-        // printf("\n");
-        return;
-    }
-    // if() {
-    //     if(pathSum < *minSum) {       
-    //     }
-    //     return;
-    // }
-    findSum(triangle, triangleSize, row + 1, numIndex, pathSum, minSum);
-    findSum(triangle, triangleSize, row + 1, numIndex + 1, pathSum, minSum);
-    // return pathSum + ;
-}
+// my first solution
+// void findSum(int **triangle, int triangleSize, int row, int numIndex, int pathSum, int *minSum) {
+//     // printf("%d ", triangle[row][numIndex]);
+//     pathSum += triangle[row][numIndex];
+//     // if(pathSum > *minSum) {
+//     //     return;
+//     // } 
+//     if((row == (triangleSize - 1))) {
+//         if(pathSum < *minSum) {
+//             *minSum = pathSum;
+//         }
+//         // printf("\n");
+//         return;
+//     }
+//     // if() {
+//     //     if(pathSum < *minSum) {       
+//     //     }
+//     //     return;
+//     // }
+//     findSum(triangle, triangleSize, row + 1, numIndex, pathSum, minSum);
+//     findSum(triangle, triangleSize, row + 1, numIndex + 1, pathSum, minSum);
+//     // return pathSum + ;
+// }
+
+// int minimumTotal(int **triangle, int triangleSize, int *triangleColSize) {
+//     // int i;
+//     // for(i = 0; i < triangleSize; i++) {
+//     //     printf("%d ", triangleColSize[i]);
+//     // }
+//     // printf("\n");
+//     // int j;
+//     // for(i = 0; i < triangleSize; i++) {
+//     //     for(j = 0; j < triangleColSize[i]; j++) {
+//     //         printf("%d ", triangle[i][j]);
+//     //     }
+//     //     printf("\n");
+//     // }
+//     int minSum = INT_MAX;
+//     findSum(triangle, triangleSize, 0, 0, 0, &minSum);
+//     return minSum;
+// }
+
+//second solution:
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
 
 int minimumTotal(int **triangle, int triangleSize, int *triangleColSize) {
-    // int i;
-    // for(i = 0; i < triangleSize; i++) {
-    //     printf("%d ", triangleColSize[i]);
-    // }
-    // printf("\n");
-    // int j;
-    // for(i = 0; i < triangleSize; i++) {
-    //     for(j = 0; j < triangleColSize[i]; j++) {
-    //         printf("%d ", triangle[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    int minSum = INT_MAX;
-    findSum(triangle, triangleSize, 0, 0, 0, &minSum);
-    return minSum;
+    int dp[triangleSize + 1][triangleSize + 1];
+    int i;
+    for(i = 0; i < triangleSize + 1; i++) {
+        dp[triangleSize][i] = 0;
+    }
+    for(int level = triangleSize - 1; level >= 0; level--) {
+        for(i = 0; i <= triangleSize; i++) {
+            dp[level][i] = triangle[level][i] + MIN(dp[level + 1][i], dp[level + 1][i + 1]);
+        }
+    }
+    return dp[0][0];
 }
 
 int main() {
-    int rows = 4;
+    // int rows = 4;
     
-    // int rows = 1;
+    int rows = 1;
     
     // int rows = 3;
 
@@ -57,18 +75,18 @@ int main() {
         *(triangle + (numbers - 1)) = (int*)malloc(numbers * sizeof(int));
         triangleColSize[numbers - 1] = numbers;
     }
-    *(*(triangle + 0) + 0) = 2;
-    *(*(triangle + 1) + 0) = 3;
-    *(*(triangle + 1) + 1) = 4;
-    *(*(triangle + 2) + 0) = 6;
-    *(*(triangle + 2) + 1) = 5;
-    *(*(triangle + 2) + 2) = 7;
-    *(*(triangle + 3) + 0) = 4;
-    *(*(triangle + 3) + 1) = 1;
-    *(*(triangle + 3) + 2) = 8;
-    *(*(triangle + 3) + 3) = 3;
+    // *(*(triangle + 0) + 0) = 2;
+    // *(*(triangle + 1) + 0) = 3;
+    // *(*(triangle + 1) + 1) = 4;
+    // *(*(triangle + 2) + 0) = 6;
+    // *(*(triangle + 2) + 1) = 5;
+    // *(*(triangle + 2) + 2) = 7;
+    // *(*(triangle + 3) + 0) = 4;
+    // *(*(triangle + 3) + 1) = 1;
+    // *(*(triangle + 3) + 2) = 8;
+    // *(*(triangle + 3) + 3) = 3;
     
-    // *(*(triangle + 0) + 0) = -10;
+    *(*(triangle + 0) + 0) = -10;
     
     // *(*(triangle + 0) + 0) = -1;
     // *(*(triangle + 1) + 0) = 2;
